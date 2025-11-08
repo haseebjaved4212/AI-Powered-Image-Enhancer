b45cd075e99099bf7ddc8930de482ed762447c89
haseebjaved4212 <m.haseebjavedhj@gmail.com>
2025-11-08 10:23:26 +0500

    Coplete A section

--- src/components/ImagePreview.jsx
+++ src/components/ImagePreview.jsx
@@ -4,7 +4,7 @@ const ImagePreview = (props) => {
   return (
     <div className="mt-8 grid grid-cols-1 md:grid-cols-2 gap-6 w-full max-w-4xl">
       {/* Original Image */}
-      <div className="bg-white shadow-lg rounded-xl overflow-hidden">
+      <div className="bg-zinc-700 shadow-lg rounded-xl overflow-hidden">
         <h2 className="text-xl font-semibold text-center bg-gray-800 text-white py-2">
           Original Image
         </h2>
@@ -16,15 +16,15 @@ const ImagePreview = (props) => {
             className="w-full h-full object-cover"
           />
         ) : (
-          <div className="flex items-center justify-center h-80 bg-gray-200">
+          <div className="flex items-center justify-center h-80 bg-zinc-700 text-white ">
             No Image Selected
           </div>
         )}
       </div>
 
       {/* Enhanced Image Image */}
-      <div className="bg-white shadow-lg rounded-xl overflow-hidden">
-        <h2 className="text-xl font-semibold text-center bg-blue-800 text-white py-2">
+      <div className="bg-zinc-700 shadow-lg rounded-xl overflow-hidden">
+        <h2 className="text-xl font-semibold text-center bg-gray-800 text-white py-2">
           Enhanced Image
         </h2>
 
@@ -39,7 +39,7 @@ const ImagePreview = (props) => {
         {props.loading ? (
           <Loading />
         ) : (
-          <div className="flex items-center justify-center h-80 bg-gray-200">
+          <div className="flex items-center justify-center h-80 bg-zinc-700 text-white">
             No Enhanced Image
           </div>
         )}
--- src/components/ImageUpload.jsx
+++ src/components/ImageUpload.jsx
@@ -7,7 +7,7 @@
         };
 
         return (
-          <div className="bg-white shadow-lg rounded-2xl p-6 w-full max-w-2xl">
+          <div className="bg-zinc-700 text-white shadow-lg rounded-2xl p-6 w-full max-w-2xl">
             <label
               htmlFor="fileInput"
               className="block w-full cursor-pointer border-2 border-dashed border-gray-300 rounded-lg p-6 text-center hover:border-blue-500 transition-all"
@@ -18,7 +18,7 @@
                 className="hidden"
                 onChange={ShowImageHandler}
               />
-              <span className="text-lg font-medium text-gray-600">
+              <span className="text-lg font-medium ">
                 Click and drag to upload your image
               </span>
             </label>
